fdf014065d4f39d757a2d1c840f91866af4888cd
ElPanaJorge03 <elpanajorge03@gmail.com>
2026-06-05 19:06:05 -0500

    cors cambiadas

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
@@ -7,6 +8,14 @@ from app.routers import ingredientes
 from app.routers import recetas
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(ingredientes.router)
--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "http://38.180.81.158:8000";
 
 const registerForm = document.getElementById("register-form");
 const loginForm = document.getElementById("login-form");
